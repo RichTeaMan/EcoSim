1dfee3185e004681017f4d7c67702fbddf6b58d6
Thomas Holmes <thomasholmes@outlook.com>
2014-05-19 23:22:35 +0100

    Added PlainWorldFormer.

---
+++ EcoSim.Logic/PlainWorldFormer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoSim.Logic
+{
+    public class PlainWorldFormer : IWorldFormer
+    {
+        public string Summary
+        {
+            get { return "Creates a plain world at the given altitude. Water will only generate at altitude below 0."; }
+        }
+
+        public short Altitude { get; set; }
+
+        public PlainWorldFormer()
+        {
+            Altitude = 5;
+        }
+
+        public void Generate(World world)
+        {
+            foreach (var p in world.Positions)
+            {
+                p.Altitude = Altitude;
+            }
+        }
+    }
+}
