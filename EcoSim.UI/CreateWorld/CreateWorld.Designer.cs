e7d76b75751a18b29698c1fc0cfe159b5452898d
Thomas Holmes <thomasholmes@outlook.com>
2014-05-19 20:53:14 +0100

    Changed world form to be tab based. Changed project architecture to be any cpu.

--- EcoSim.UI/CreateWorld/CreateWorld.Designer.cs
+++ EcoSim.UI/CreateWorld/CreateWorld.Designer.cs
@@ -28,86 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
-            "Creatures",
-            "1"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Flora");
-            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("World");
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.ListOptions = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.but_OK = new System.Windows.Forms.Button();
             this.but_Cancel = new System.Windows.Forms.Button();
-            this.SwitchPanel = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.creaturesCtrl = new EcoSim.UI.CreaturesCreateWorld();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.floraCtrl = new EcoSim.UI.FloraCreateWorld();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.worldCtrl = new EcoSim.UI.WorldCreateWorld();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.55263F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.44736F));
-            this.tableLayoutPanel1.Controls.Add(this.ListOptions, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.SwitchPanel, 1, 0);
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tabControl1, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(456, 349);
             this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // ListOptions
-            // 
-            this.ListOptions.Alignment = System.Windows.Forms.ListViewAlignment.Left;
-            this.ListOptions.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1});
-            this.ListOptions.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ListOptions.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1,
-            listViewItem2,
-            listViewItem3});
-            this.ListOptions.LabelWrap = false;
-            this.ListOptions.Location = new System.Drawing.Point(3, 3);
-            this.ListOptions.Name = "ListOptions";
-            this.ListOptions.Size = new System.Drawing.Size(147, 309);
-            this.ListOptions.TabIndex = 1;
-            this.ListOptions.UseCompatibleStateImageBehavior = false;
-            this.ListOptions.View = System.Windows.Forms.View.Details;
-            this.ListOptions.SelectedIndexChanged += new System.EventHandler(this.ListOptions_SelectedIndexChanged);
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "Options";
-            this.columnHeader1.Width = 142;
             // 
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.ColumnCount = 3;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.56158F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.43842F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.44566F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.55435F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 82F));
             this.tableLayoutPanel2.Controls.Add(this.but_OK, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.but_Cancel, 2, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(156, 318);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 318);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(297, 28);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(450, 28);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
             // but_OK
             // 
             this.but_OK.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.but_OK.Location = new System.Drawing.Point(138, 3);
+            this.but_OK.Location = new System.Drawing.Point(287, 3);
             this.but_OK.Name = "but_OK";
-            this.but_OK.Size = new System.Drawing.Size(75, 22);
+            this.but_OK.Size = new System.Drawing.Size(76, 22);
             this.but_OK.TabIndex = 0;
             this.but_OK.Text = "OK";
             this.but_OK.UseVisualStyleBackColor = true;
@@ -116,21 +93,81 @@
             // but_Cancel
             // 
             this.but_Cancel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.but_Cancel.Location = new System.Drawing.Point(219, 3);
+            this.but_Cancel.Location = new System.Drawing.Point(369, 3);
             this.but_Cancel.Name = "but_Cancel";
-            this.but_Cancel.Size = new System.Drawing.Size(75, 22);
+            this.but_Cancel.Size = new System.Drawing.Size(78, 22);
             this.but_Cancel.TabIndex = 1;
             this.but_Cancel.Text = "Cancel";
             this.but_Cancel.UseVisualStyleBackColor = true;
             this.but_Cancel.Click += new System.EventHandler(this.but_Cancel_Click);
             // 
-            // SwitchPanel
+            // tabControl1
             // 
-            this.SwitchPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.SwitchPanel.Location = new System.Drawing.Point(156, 3);
-            this.SwitchPanel.Name = "SwitchPanel";
-            this.SwitchPanel.Size = new System.Drawing.Size(297, 309);
-            this.SwitchPanel.TabIndex = 3;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(450, 309);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.creaturesCtrl);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(442, 283);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Creatures";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // creaturesCtrl
+            // 
+            this.creaturesCtrl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.creaturesCtrl.Location = new System.Drawing.Point(3, 3);
+            this.creaturesCtrl.Name = "creaturesCtrl";
+            this.creaturesCtrl.Size = new System.Drawing.Size(436, 277);
+            this.creaturesCtrl.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.floraCtrl);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(442, 283);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Flora";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // floraCtrl
+            // 
+            this.floraCtrl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.floraCtrl.Location = new System.Drawing.Point(3, 3);
+            this.floraCtrl.Name = "floraCtrl";
+            this.floraCtrl.Size = new System.Drawing.Size(436, 277);
+            this.floraCtrl.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.worldCtrl);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(442, 283);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "World";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // worldCtrl
+            // 
+            this.worldCtrl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.worldCtrl.Location = new System.Drawing.Point(0, 0);
+            this.worldCtrl.Name = "worldCtrl";
+            this.worldCtrl.Size = new System.Drawing.Size(442, 283);
+            this.worldCtrl.TabIndex = 0;
             // 
             // CreateWorld
             // 
@@ -142,6 +179,10 @@
             this.Text = "CreateWorld";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -149,12 +190,15 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.ListView ListOptions;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.Button but_OK;
         private System.Windows.Forms.Button but_Cancel;
-        private System.Windows.Forms.Panel SwitchPanel;
-
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private CreaturesCreateWorld creaturesCtrl;
+        private FloraCreateWorld floraCtrl;
+        private WorldCreateWorld worldCtrl;
     }
 }
--- EcoSim.UI/CreateWorld/CreateWorld.cs
+++ EcoSim.UI/CreateWorld/CreateWorld.cs
@@ -16,52 +16,16 @@ namespace EcoSim.UI
 
         public World CreatedWorld { get; private set; }
 
-        private Control OldPanel;
-        private CreaturesCreateWorld Creatures;
-        private FloraCreateWorld Flora;
-        private WorldCreateWorld World;
-
         public CreateWorld()
         {
             InitializeComponent();
-
-            if (DesignMode == false)
-            {
-                Creatures = new CreaturesCreateWorld();
-                Flora = new FloraCreateWorld();
-                World = new WorldCreateWorld();
-
-                OldPanel = Creatures;
-                SwitchPanel.Controls.Add(OldPanel);
-            }
-        }
-
-        private void ListOptions_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (ListOptions.SelectedItems.Count != 0)
-            {
-                SwitchPanel.Controls.Remove(OldPanel);
-                switch (ListOptions.SelectedItems[0].Text)
-                {
-                    case "Creatures":
-                        OldPanel = Creatures;
-                        break;
-                    case "Flora":
-                        OldPanel = Flora;
-                        break;
-                    case "World":
-                        OldPanel = World;
-                        break;
-                }
-                SwitchPanel.Controls.Add(OldPanel);
-            }
         }
 
         private void but_OK_Click(object sender, EventArgs e)
         {
-            var newWorld = new World(World.WorldWidth, World.WorldHeight, World.WorldWrap);
-            newWorld.InitialiseFlora(Flora.InitialCoverage);
-            newWorld.InitialiseCreatures(Creatures.StartPopulation, Creatures.MaxPopulation);
+            var newWorld = new World(worldCtrl.WorldWidth, worldCtrl.WorldHeight, worldCtrl.WorldWrap);
+            newWorld.InitialiseFlora(floraCtrl.InitialCoverage);
+            newWorld.InitialiseCreatures(creaturesCtrl.StartPopulation, creaturesCtrl.MaxPopulation);
 
             CreatedWorld = newWorld;
 
